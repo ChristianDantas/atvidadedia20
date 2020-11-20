4aec7cea7ae4b5e0d853a16716ddf78a6bc371b3
DEV <dev@info.local>
2020-11-20 16:48:29 -0300

    vuifbfiufdsgnlkrjsenrgilu

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace atvidadedia20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        
+           string[] nomesa= new string[10];
+            double[] nota1= new double[10];
+            double[] nota2= new double[10];
+            double[] nota3= new double[10];
+            double[] nota4= new double[10];
+            double[] media= new double[10];
+
+            for (var i = 0; i <10; i++)
+            {
+                Console.WriteLine("Digite o nome");
+                nomesa[i]=Console.ReadLine();
+                Console.WriteLine("Digite a primeira nota");
+                nomesa[i]=Console.ReadLine();
+                Console.WriteLine("Digite a segunda nota");
+                nomesa[i]=Console.ReadLine();
+                Console.WriteLine("Digite a terceira nota");
+                nomesa[i]=Console.ReadLine();
+                Console.WriteLine("Digite a quarta nota");
+                nomesa[i]=Console.ReadLine();
+                media[i]= ( nota1[] +  nota2[] + nota3 + nota4) /2;
+            }
+
+
+            
+          
+
+        }
+    }
+}
